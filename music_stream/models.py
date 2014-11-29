957815dae40865919d8256085750694c4a6eafc8
Kruz Kalke <kruzkalke@hotmail.com>
2014-11-28 20:54:07 -0600

    Songfiles Are Saved When MD5 Is Unique

--- music_stream/models.py
+++ music_stream/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 from django.db.models import Model
+from django.core.files.storage import FileSystemStorage
 from os.path import splitext
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3, TIT2, TPE1, COMM
+import hashlib
+
+class CustomStorage(FileSystemStorage):
+	def get_available_name(self, file_name):
+		return file_name
+		
+	def _save(self, file_name, content):
+		if self.exists(file_name):
+			#if the file exists, do not save the new file
+			return file_name
+		#if the file does not exist, save the file
+		return super(CustomStorage, self)._save(file_name, content)		
 
 class Music(models.Model):
 	name = models.CharField(max_length=128,unique=True)
@@ -19,17 +32,23 @@ class Music(models.Model):
 
 class Song(models.Model):
 	owner = models.CharField(max_length=30,default=None)
-	file_name = models.CharField(max_length=128,unique=True)
-	songfile = models.FileField(upload_to='music/%Y/%m/%d')
+	file_name = models.CharField(max_length=128)
+	songfile = models.FileField(upload_to='music/%Y/%m/%d',storage=CustomStorage())
+	md5sum = models.CharField(max_length=36)
 	title = models.CharField(max_length=128,default='untitled')
 	artist = models.CharField(max_length=128,default='unknown')
 	track_num = models.PositiveSmallIntegerField(default=1)
 
 	def __unicode__(self):
-			return self.file_name
+		return self.file_name
 
 	def save(self, *args, **kwargs):
-		self.file_name = splitext(self.file_name)[0]
+		#self.file_name = splitext(self.file_name)[0]
+		if not self.pk: #file is new
+			md5 = hashlib.md5()
+			for chunk in self.songfile.chunks():
+					md5.update(chunk)
+			self.md5sum = md5.hexdigest()
 		super(Song,self).save(*args, **kwargs)
 
 	def update(self):
@@ -40,6 +59,4 @@ class Song(models.Model):
 		tmp = audiofile["TRCK"]
 		tmp = str(tmp).split("/",1)
 		self.track_num = tmp[0]
-		super(Song,self).save();
-
-		
+		super(Song,self).save()
