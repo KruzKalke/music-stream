ac47e8dc5612f6faef5f083b7474a8895aa62b95
giffarage <tylermassey6@gmail.com>
2014-11-28 22:38:31 -0600

    Updates:   - To the Model     - added new fields to the song Object     - added slug fields for album and artist (this is for URLs)   - Added search functionality     - searching returns matches for substrings in artists, albums, and songs     - the results for artists and albums are links     - artist pages are created on the fly and list all albums and songs by that artist     - album pages are also created on the fly and list all songs in that album     - artist and album pages can be accessed directly without needing to search        - going to any localhost:8000/artist/<artistname> or localhost:8000/album/<albumname> automatically creates and displays that page   - For implementing search, I created three new views, three new templates, and I also made changes to urls.py

--- music_stream/models.py
+++ music_stream/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.db.models import Model
+from django.template.defaultfilters import slugify
 from os.path import splitext
 from mutagen.mp3 import MP3
-from mutagen.id3 import ID3, TIT2, TPE1, COMM, TIME
+from mutagen.id3 import ID3, TIT2, TPE1, COMM, TALB
 
 
 class Song(models.Model):
@@ -11,8 +12,12 @@ class Song(models.Model):
 	songfile = models.FileField(upload_to='music/%Y/%m/%d')
 	title = models.CharField(max_length=128,default='untitled')
 	artist = models.CharField(max_length=128,default='unknown')
+	album = models.CharField(max_length=128,default='unknown')
 	track_num = models.PositiveSmallIntegerField(default=1)
 	length = models.PositiveSmallIntegerField(default=0)
+	artist_slug = models.SlugField(max_length=128,default='unknown')
+	album_slug = models.SlugField(max_length=128,default='unknown')
+
 
 	def __unicode__(self):
 			return self.file_name
@@ -26,6 +31,9 @@ class Song(models.Model):
 		mp3file = MP3(self.songfile.path)
 		self.title = audiofile["TIT2"]
 		self.artist = audiofile["TPE1"]
+		self.album = audiofile["TALB"]
+		self.artist_slug = slugify(self.artist)
+		self.album_slug = slugify(self.album)
 		tmp = mp3file.info.length
 		tmp = str(tmp).split(".",1)
 		self.length = tmp[0]
